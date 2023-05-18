d974dcf4083a850fbda325e0bb915f8a774c94bb
mw-wsb <wrx75709@student.wsb.wroclaw.pl>
2023-05-19 00:10:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>Hello WSB! Greetings from Flask!</h1>'
+	return '<h1>Hello WSB! Greetings from Flask</h1>'
 
 
 if __name__ == "__main__":
